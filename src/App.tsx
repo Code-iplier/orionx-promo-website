e9d88233cada68c283e9cccd6f5c626fbfede71f
Code-iplier <bioh4z3rd21@gmail.com>
2026-06-29 20:02:37 +0530

    [AL14] Initial Commit Scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/fOTAcra3J1e7oPUb/scene.splinecode" />
+  );
+}
+
+
+
